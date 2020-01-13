a9b0264faecc247d2c5eb0485d2332a3f692ecdf
Atsushi Shiraishi <shira7867@gmail.com>
2020-01-13 17:44:59 +0900

    input from stdin

--- draw.py
+++ draw.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 from PIL import Image
 import numpy as np
 
@@ -8,22 +9,28 @@ def mapv(v, s1, e1, s2, e2):
     return s2 + (e2 - s2) * ((v - s1) / (e1 - s1))
 
 
-screen = []
-min_n, max_n = 99999999, 0
-for line in open('out.txt', 'r'):
-    ls = []
-    for s in line.split(','):
-        n = int(s)
-        min_n = n if n < min_n else min_n
-        max_n = n if n > max_n else max_n
-        ls.append(n)
-    screen.append(ls)
+if __name__ == '__main__':
+    if len(sys.argv) <= 1 or len(sys.argv[1]) <= 0:
+        print('usage: ./draw.py out.png < out.txt')
+        exit(1)
 
-H, W = len(screen), len(screen[0])
+    filename = sys.argv[1]
+    screen = []
+    min_n, max_n = sys.maxsize, 0
+    for line in sys.stdin:
+        ls = []
+        for s in line.split(','):
+            n = int(s)
+            min_n = n if n < min_n else min_n
+            max_n = n if n > max_n else max_n
+            ls.append(n)
+        screen.append(ls)
 
-for y in range(H):
-    for x in range(W):
-        screen[y][x] = mapv(screen[y][x], min_n, max_n, 0, 255)
+    H, W = len(screen), len(screen[0])
 
-img = Image.fromarray(np.array(screen).astype(np.uint8))
-img.save('out.jpg')
+    for y in range(H):
+        for x in range(W):
+            screen[y][x] = mapv(screen[y][x], min_n, max_n, 0, 255)
+
+    img = Image.fromarray(np.array(screen).astype(np.uint8))
+    img.save(filename)
